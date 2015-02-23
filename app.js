9df7f2f9a45fa242c5296229862c9e34dba0cab4
Rice <rice90128@gmail.com>
2015-02-22 22:43:06 -0800

    ''

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
+/*app.use(session({cookie: {maxAge: 3600000 },secret: 'something'}));*/
 app.use(cookieSession({ keys: ['secretkey1', 'secretkey2', '...']}));
 // passport config
 var Account = require('./models/user');
